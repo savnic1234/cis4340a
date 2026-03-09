f9bd652ef13f647efe21e9903689fbfedc949bbd
savnic1234 <savannawrye@att.net>
2026-03-08 22:27:53 -0400

    XP00-J. Do not ignore values returned by methods

---
+++ R02_XP00_J.java
@@ -0,0 +1,20 @@
+// Rule 02. Expressions (EXP)
+// XP00-J. Do not ignore values returned by methods
+
+public void deleteFile(){
+
+  File someFile = new File("someFileName.txt");
+  // Do something with someFile
+  someFile.delete();
+
+}
+
+public void deleteFile(){
+
+  File someFile = new File("someFileName.txt");
+  // Do something with someFile
+  if (!someFile.delete()) {
+    // Handle failure to delete the file
+  }
+
+}
