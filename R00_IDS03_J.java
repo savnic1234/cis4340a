ee14729f6dd2b7d29e98bfbe7a340b42ea0b633e
savnic1234 <savannawrye@att.net>
2026-03-08 22:28:49 -0400

    IDS03-J. Do not log unsanitized user input

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,3 +1,6 @@
+// Rule 00. Input Validation and Data Sanitization (IDS)
+// IDS03-J. Do not log unsanitized user input
+
 if (loginSuccessful) {
   logger.severe("User login succeeded for: " + username);
 } else {
