5efe557f17289e450cf5f7be790c3267f9405f84
savnic1234 <savannawrye@att.net>
2026-03-08 22:39:44 -0400

    LCK03-J. Do not synchronize on the intrinsic locks of high-level concurrency objects

---
+++ R09_LCK03_J.java
@@ -0,0 +1,24 @@
+// Rule 09. Locking (LCK)
+// LCK03-J. Do not synchronize on the intrinsic locks of high-level concurrency objects
+
+
+private final Lock lock = new ReentrantLock();
+
+public void doSomething() {
+  synchronized(lock) {
+    // ...
+  }
+}
+
+
+
+private final Lock lock = new ReentrantLock();
+
+public void doSomething() {
+  lock.lock();
+  try {
+    // ...
+  } finally {
+    lock.unlock();
+  }
+}
