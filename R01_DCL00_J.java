3d1ceaa67e1a50d37d407e629248923871d80c0c
savnic1234 <savannawrye@att.net>
2026-03-08 22:36:20 -0400

    DCL00-J. Prevent class initialization cycles

---
+++ R01_DCL00_J.java
@@ -0,0 +1,31 @@
+// Rule 01. Declarations and Initialization (DCL)
+// DCL00-J. Prevent class initialization cycles
+
+public class Cycle {
+  private final int balance;
+  private static final Cycle c = new Cycle();
+  private static final int deposit = (int) (Math.random() * 100); // Random deposit
+
+  public Cycle() {
+    balance = deposit - 10; // Subtract processing fee
+  }
+
+  public static void main(String[] args) {
+    System.out.println("The account balance is: " + c.balance);
+  }
+}
+
+
+public class Cycle {
+  private final int balance;
+  private static final int deposit = (int) (Math.random() * 100); // Random deposit
+  private static final Cycle c = new Cycle();  // Inserted after initialization of required fields
+  public Cycle() {
+    balance = deposit - 10; // Subtract processing fee
+  }
+
+  public static void main(String[] args) {
+    System.out.println("The account balance is: " + c.balance);
+  }
+}
+
