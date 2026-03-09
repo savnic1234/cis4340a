3d47f751ee7c9426d3093ec4cdd05c8a35a5e03d
savnic1234 <savannawrye@att.net>
2026-03-08 22:41:45 -0400

    THI00-J. Do not invoke Thread.run()

---
+++ R10_THI00_J.java
@@ -0,0 +1,26 @@
+// Rule 10. Thread APIs (THI)
+// THI00-J. Do not invoke Thread.run()
+
+public final class Foo implements Runnable {
+  @Override public void run() {
+    // ...
+  }
+
+  public static void main(String[] args) {
+    Foo foo = new Foo();
+    new Thread(foo).run();
+  }
+}
+
+
+
+public final class Foo implements Runnable {
+  @Override public void run() {
+    // ...
+  }
+
+  public static void main(String[] args) {
+    Foo foo = new Foo();
+    new Thread(foo).start();
+  }
+}
